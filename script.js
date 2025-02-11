925a543fdf6479d44cd949ce09b7cde460bddf88
Srushti-K-01 <ksrushtimahesh@gmail.com>
2025-02-11 11:36:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const student = {
+	name: "John",
+};
+function getKeys(obj){
+	return Object.keys(obj);
+}
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
+};
+console.log(getKeys(student));
